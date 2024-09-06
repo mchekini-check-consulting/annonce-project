4e8b790b14c71077749f10a89cc596d8fca4d619
arris-ismahane <arrisismahane@gmail.com>
2024-09-06 17:49:40 +0200

    Feature/IHM/product-list-ui affichage des annonces

--- annonce-ihm/src/app/app.component.ts
+++ annonce-ihm/src/app/app.component.ts
@@ -1,25 +1,26 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {HttpClient} from "@angular/common/http";
-
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-
-  constructor(private http: HttpClient) {
-
+  ngOnInit(): void {
+    this.loadDate();
   }
 
+  annonces: any = [];
   loadDate() {
-    this.http.get("data.json").subscribe(response => {
-      console.log(response);
-    })
+    this.http.get('data.json').subscribe((response) => {
+      this.annonces = response;
 
+    });
   }
 }
