c88ddced806c0b30de5e45cc03e9c37eb26eabba
mahdi chekini <mchekini@checkconsulting.fr>
2024-09-06 13:02:39 +0200

    init annonce project

---
+++ annonce-api/src/main/java/fr/checkconsulting/annonceapi/AnnonceApiApplication.java
@@ -0,0 +1,13 @@
+package fr.checkconsulting.annonceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnonceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnonceApiApplication.class, args);
+    }
+
+}
---
+++ annonce-api/src/test/java/fr/checkconsulting/annonceapi/AnnonceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.checkconsulting.annonceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnonceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
