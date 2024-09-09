c6a5e5a8a99453729ee49a7cfb4a06a8d1afa98f
Moh-Tacine <mytacine.dev@gmail.com>
2024-09-09 17:56:40 +0200

    Suppression d'une annonce par ID Ajout de l'icone supprimer Ajout d'une fenêtre de confirmation de la suppression Signed-off-by: Moh-Tacine <mytacine.dev@gmail.com>

--- annonce-ihm/src/app/app.component.ts
+++ annonce-ihm/src/app/app.component.ts
@@ -1,20 +1,28 @@
-import {ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
-import {AnnonceModel} from "./core/model/annonce.model";
-import {AnnonceService} from "./core/service/annonce.service";
-import {SearchCriteriaModel} from "./core/model/search.criteria.model";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatTableDataSource, MatTableModule} from "@angular/material/table";
-import {MatSortModule, Sort} from "@angular/material/sort";
-import {MatPaginatorModule, PageEvent} from "@angular/material/paginator";
-import {DatePipe, JsonPipe, NgForOf} from "@angular/common";
-import {FormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from "@angular/material/core";
-import {MatCardModule} from "@angular/material/card";
-import {MatSelectModule} from "@angular/material/select";
-import {MatDivider} from "@angular/material/divider";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  OnInit,
+} from '@angular/core';
+import { AnnonceModel } from './core/model/annonce.model';
+import { AnnonceService } from './core/service/annonce.service';
+import { SearchCriteriaModel } from './core/model/search.criteria.model';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSortModule, Sort } from '@angular/material/sort';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { DatePipe, JsonPipe, NgForOf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDivider } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import {MatDialog} from "@angular/material/dialog";
+import {DeleteDialogComponent} from "./core/delete-dialog/delete-dialog.component";
 
 @Component({
   selector: 'app-root',
@@ -26,7 +34,7 @@ import {MatDivider} from "@angular/material/divider";
     MatButtonModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCardModule, MatDivider, NgForOf],
+    MatCardModule, MatDivider, NgForOf, MatIconModule],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ],
@@ -35,8 +43,7 @@ import {MatDivider} from "@angular/material/divider";
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-
-  displayedColumns: string[] = ['title', 'description', 'price', 'category', 'Localisation', 'postedAt'];
+  displayedColumns: string[] = ['title', 'description', 'price', 'category', 'Localisation', 'postedAt', 'actions'];
   searchCriteria = new SearchCriteriaModel();
   annonces = new MatTableDataSource<AnnonceModel>();
   categories: string[] = [
@@ -48,8 +55,7 @@ export class AppComponent implements OnInit {
     'VETEMENTS'
   ];
 
-  constructor(private annonceService: AnnonceService) {
-
+  constructor(private annonceService: AnnonceService, private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
@@ -87,5 +93,25 @@ export class AppComponent implements OnInit {
     console.log(this.searchCriteria);
     this.loadData();
 
+  }
+
+  deleteRow(id: number) {
+    this.openDialog().afterClosed().subscribe(value => {
+      if (value) {
+        this.annonceService.deleteAnnonceById(id).subscribe(() => {
+          this.annonces.data = this.annonces.data.filter(
+            (annonce) => annonce.annonceId !== id
+          );
+        });
+      }
+    })
+  }
+
+  openDialog() {
+    return this.dialog.open(DeleteDialogComponent, {
+      width: '450px',
+      enterAnimationDuration: '0ms',
+      exitAnimationDuration: '0ms'
+    });
   }
 }
--- annonce-ihm/src/app/core/service/annonce.service.ts
+++ annonce-ihm/src/app/core/service/annonce.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {AnnonceModel} from "../model/annonce.model";
-import {Page} from "../model/page.model";
-import {SearchCriteriaModel} from "../model/search.criteria.model";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AnnonceModel } from '../model/annonce.model';
+import { Page } from '../model/page.model';
+import { SearchCriteriaModel } from '../model/search.criteria.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnnonceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
+  searchAnnonces(body: SearchCriteriaModel): Observable<Page<AnnonceModel>> {
+    return this.http.post<Page<AnnonceModel>>('http://52.23.192.75:8080/api/v1/annonce/search', body);
   }
 
-  searchAnnonces(body : SearchCriteriaModel) : Observable<Page<AnnonceModel>> {
-     return this.http.post<Page<AnnonceModel>>('http://52.23.192.75:8080/api/v1/annonce/search', body);
+  deleteAnnonceById(id: number): Observable<void> {
+    return this.http.delete<void>('http://52.23.192.75:8080/api/v1/annonce/' + id);
   }
 }
