06083c27a2a124daf9817a242dd97166fe1f946f
mahdi chekini <mchekini@checkconsulting.fr>
2024-09-10 17:40:43 +0200

    - Add Annonce Management Component

--- annonce-ihm/src/app/features/annonce-management/annonce-management.component.ts
+++ annonce-ihm/src/app/features/annonce-management/annonce-management.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {AnnonceItemComponent} from "./components/annonce-item/annonce-item.component";
 
 @Component({
   selector: 'app-annonce-management',
   standalone: true,
-  imports: [],
+  imports: [
+    AnnonceItemComponent
+  ],
   templateUrl: './annonce-management.component.html',
   styleUrl: './annonce-management.component.css'
 })
 export class AnnonceManagementComponent {
 
+  displayInformation(message: string) {
+    console.log(message);
+  }
 }
---
+++ annonce-ihm/src/app/features/annonce-management/components/annonce-item/annonce-item.component.ts
@@ -0,0 +1,12 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-annonce-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './annonce-item.component.html',
+  styleUrl: './annonce-item.component.css'
+})
+export class AnnonceItemComponent {
+
+}
