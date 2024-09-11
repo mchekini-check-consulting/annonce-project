3bbdaa2e093abb2721b46a6cf786cc70b8a128fc
tadix <linubiame@gmail.com>
2024-09-11 03:15:57 +0100

    Fix for the marketplace screen Added the details screen for annonce Added the annonce data binding service

--- annonce-ihm/src/app/app.routes.ts
+++ annonce-ihm/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import {Routes} from '@angular/router';
 import {GestionAnnoncesComponent} from "./features/gestion-annonces/gestion-annonces.component";
 import {StatistiquesComponent} from "./features/statistiques/statistiques.component";
 import {AnnonceManagementComponent} from "./features/annonce-management/annonce-management.component";
+import {
+  AnnonceDetailsComponent
+} from "./features/annonce-management/components/annonce-details/annonce-details.component";
 
 export const routes: Routes = [
   {path: '', component: GestionAnnoncesComponent},
   {path: 'gestion-annonces', component: GestionAnnoncesComponent},
   {path: 'statistiques', component: StatistiquesComponent},
   {path: 'annonce-management', component: AnnonceManagementComponent},
+  {path: 'annonce-details', component: AnnonceDetailsComponent},
 
 ];
--- annonce-ihm/src/app/features/annonce-management/annonce-management.component.ts
+++ annonce-ihm/src/app/features/annonce-management/annonce-management.component.ts
@@ -1,9 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {AnnonceItemComponent} from "./components/annonce-item/annonce-item.component";
 import {AnnonceModel} from "../../core/model/annonce.model";
 import {AnnonceService} from "../../core/service/annonce.service";
 import {SearchCriteriaModel} from "../../core/model/search.criteria.model";
 import { CommonModule } from '@angular/common';
+import {Router} from "@angular/router";
+import {AnnonceDatabindingService} from "../../core/service/annonce-databinding.service";
 @Component({
   selector: 'app-annonce-management',
   standalone: true,
@@ -17,18 +19,25 @@ import { CommonModule } from '@angular/common';
 export class AnnonceManagementComponent implements  OnInit{
   searchCriteria = new SearchCriteriaModel();
   annonces!:AnnonceModel[];
+  selectedAnnonce:AnnonceModel|undefined;
 
-  constructor(private annonceService:AnnonceService) {
-   // this.annonces.push(this.annonceTest);
+  constructor(private annonceService:AnnonceService,
+              private cdr: ChangeDetectorRef,
+              private router: Router,
+              private annonceDatabindingService: AnnonceDatabindingService,) {
   }
   ngOnInit():void{
     this.searchCriteria.pageSize = 10;
     this.annonceService.searchAnnonces(this.searchCriteria).subscribe(response => {
       this.annonces = response.content;
+      this.cdr.detectChanges();
     });
   }
-  itemTitleClick(title:string){
-    console.log(`this is the clicked title '${title}'`);
+  itemTitleClick(annonce_id:number){
+    this.selectedAnnonce=this.annonces.find(annonce => annonce.annonceId === annonce_id);
+    this.annonceDatabindingService.setAnnonce(this.selectedAnnonce);
+    this.router.navigate(['/annonce-details']);
+
   }
 
 }
